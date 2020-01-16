0a16e14b326a7f034f7185a827ffd51462837db7
Paul Moya <paulmoyag@gmail.com>
2020-01-16 16:59:51 -0500

    subiendo cambios

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,67 +1,72 @@
-let jwt = require('jsonwebtoken');
-let jwkToPem = require('jwk-to-pem');
-let axios = require('axios');
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+const AWS = require('aws-sdk');
+const cognitoIdentity = new AWS.CognitoIdentity();
+const config = require('config-yml');
 
-exports.handler = async function(event) {
-  try {
-    let encodedToken = getEncodedToken(event.authorizationToken)
-    let token = jwt.decode(encodedToken, {complete: true});
-    let jwk = await getJwkByKid(token.payload.iss, token.header.kid);
-    let pem = jwkToPem(jwk);
-    jwt.verify(encodedToken, pem);
+global.fetch = require("node-fetch");
+AWS.config.region = 'us-east-2';
+
+module.exports.handler = async () => {
+  /*
+  console.log(config.desa.username);
+  console.log(config.desa.password);
+  console.log(config.desa.userPoolId);
+  console.log(config.desa.clientId);
+  console.log(config.desa.identityPoolId);
+  */
+  const authenticationData = {
+    Username: config.desa.username,
+    Password: config.desa.password,
+  };
+  const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+  const poolData = {
+    UserPoolId: config.desa.userPoolId,
+    ClientId: config.desa.clientId,
+  };
+  const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+  const userData = {
+    Username: config.desa.username,
+    Pool: userPool
+  };
+  const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+  cognitoUser.authenticateUser(authenticationDetails, {
+    onSuccess: function (result) {
+      const accessToken = result.getAccessToken().getJwtToken();
+      /**
+       * Use the idToken for Logins Map when Federating User Pools 
+       * with identity pools or when passing through an Authorization Header to an API Gateway Authorizer 
+       */
+      const idToken = result.idToken.jwtToken;
+      console.log('idToken', idToken);
       
-      return allowPolicy(event.methodArn);
-    } catch (error) {
-        console.error(error.message);
-        return denyAllPolicy();
-    }
-}
+       const idParams = {
+        IdentityPoolId: config.desa.identityPoolId,
+        Logins: {
+           'cognito-idp.us-east-2.amazonaws.com/us-east-2_UvhEsWOPJ': idToken
+         }
+       };
+	  
+      cognitoIdentity.getId(idParams, function (err, identity) {
+        if (err) console.log(err, err.stack); // an error occurred
+        // else console.log(identity);
+        const credentialsParams = {
+          IdentityId: identity.IdentityId,
+          Logins: {
+            'cognito-idp.us-east-2.amazonaws.com/us-east-2_UvhEsWOPJ': idToken
+          }
+        };
+        cognitoIdentity.getCredentialsForIdentity(credentialsParams, function (err, data) {
+          if (err) console.log(err, err.stack); // an error occurred
+          console.log(data);           // successful response
+        });
+      })
+    },
 
-function getEncodedToken(header) {
-    let token = header.split(" ")[1];
-    return token;
-}
+    onFailure: function (err) {
+      console.error(err);
+    },
 
-async function getJwkByKid(iss, kid) {
-  let jwksendpoint = iss + "/.well-known/jwks.json";
-  let json = await axios(jwksendpoint);
+  });
+};
 
-  for (let index = 0; index < json.data.keys.length; index++) {
-    let key = json.data.keys[index];
-
-    if(key.kid === kid)
-      return key;
-  }
-}
-
-function denyAllPolicy(){
-    return {
-        "principalId": "*",
-        "policyDocument": {
-            "Version": "2012-10-17",
-            "Statement": [
-                {
-                    "Action": "*",
-                    "Effect": "Deny",
-                    "Resource": "*"
-                }
-            ]
-        }
-    }
-}
-
-function allowPolicy(methodArn){
-    return {
-        "principalId": "apigateway.amazonaws.com",
-        "policyDocument": {
-            "Version": "2012-10-17",
-            "Statement": [
-                {
-                    "Action": "execute-api:Invoke",
-                    "Effect": "Allow",
-                    "Resource": methodArn
-                }
-            ]
-        }
-    }
-}
+//this.handler();
